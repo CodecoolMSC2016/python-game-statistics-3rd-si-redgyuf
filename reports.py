d85755fca2792c9a603d7e5d0a9b44737161056d
Daniel Gal <faramirex070@gmail.com>
2016-11-23 13:05:28 +0100

    decide() implemented - passed

--- reports.py
+++ reports.py
@@ -1,24 +1,46 @@
 def count_games(file_name):
+    counter = 0
+
+    with open(file_name, mode="r") as my_file:
+        for lines in my_file:
+            counter += 1
+
+    return counter
 
 
 def decide(file_name, year):
+    list_of_games = []
+
+    with open(file_name, mode="r") as my_file:
+        for lines in my_file:
+            list_of_games.append(lines.replace('\n', "").split(sep='\t'))
+
+    for game in list_of_games:
+        if int(game[2]) == year:
+            return True
+    return False
 
 
 def get_latest(file_name):
+    return None
 
 
 def count_by_genre(file_name, genre):
+    return None
 
 
 def get_line_number_by_title(file_name, title):
+    return None
 
-    # Bonus
 
-
+#  Bonus
 def sort_abs(file_name):
+    return None
 
 
 def get_genres(file_name):
+    return None
 
 
 def when_was_top_sold_fps(file_name):
+    return None
