5ad898449b012a8e03b57e82e0e26d7f9592b51f
Daniel Gal <faramirex070@gmail.com>
2016-11-23 12:44:49 +0100

    count_games funct implemented

--- reports.py
+++ reports.py
@@ -1,2 +1,24 @@
+def count_games(file_name):
 
-# Report functions
+
+def decide(file_name, year):
+
+
+def get_latest(file_name):
+
+
+def count_by_genre(file_name, genre):
+
+
+def get_line_number_by_title(file_name, title):
+
+    # Bonus
+
+
+def sort_abs(file_name):
+
+
+def get_genres(file_name):
+
+
+def when_was_top_sold_fps(file_name):
