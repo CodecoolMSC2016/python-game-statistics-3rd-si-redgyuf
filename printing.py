2cfa96cfbb5024afcb04b1e861be8fd38ee43c8c
Daniel Gal <faramirex070@gmail.com>
2016-11-24 12:55:24 +0100

    init printing.py

--- printing.py
+++ printing.py
@@ -1,2 +1,34 @@
+from reports import *
 
-# Printing functions
+
+def main():
+    print("How many games are in the file?")
+    print("Answer: " + str(count_games("game_stat.txt")))
+
+    print("\nIs there a game from a given year?")
+    year = int(input("Enter the year: "))
+    print("Answer: " + str(decide("game_stat.txt", year)))
+
+    print("\nWhich was the latest game?")
+    print("Answer: " + get_latest("game_stat.txt"))
+
+    print("\nHow many games do we have by genre?")
+    genre = input("Enter the genre: ")
+    print("Answer: " + str(count_by_genre("game_stat.txt", genre)))
+
+    print("\nWhat is the line number of the given game (by title)?")
+    title = input("Enter the title: ")
+    print("Answer: " + str(get_line_number_by_title("game_stat.txt", title)))
+
+    print("\nWhat is the alphabetic ordered list of the titles?")
+    print(sort_abc("game_stat.txt"))
+
+    print("\nWhat are the genres?")
+    print(get_genres("game_stat.txt"))
+
+    print("\nWhat is the release date of the top sold First-person shooter game?")
+    print("Answer: " + str(when_was_top_sold_fps("game_stat.txt")))
+
+
+if __name__ == "__main__":
+    main()
