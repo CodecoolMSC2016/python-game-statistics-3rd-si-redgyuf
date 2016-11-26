fd31830339d2525e0dbbe21512c0e43422097a43
Daniel Gal <faramirex070@gmail.com>
2016-11-26 13:27:41 +0100

    sum_sold() implemented - passed

--- part2/reports.py
+++ part2/reports.py
@@ -16,7 +16,17 @@ def get_most_played(file_name):
 
 
 def sum_sold(file_name):
-    pass
+    list_of_games = []
+    total_sold = 0
+
+    with open(file_name, mode="r") as my_file:
+        for lines in my_file:
+            list_of_games.append(lines.replace('\n', "").split(sep='\t'))
+
+    for i in range(0, len(list_of_games)):
+        total_sold += float(list_of_games[i][1])
+
+    return total_sold
 
 
 def get_selling_avg(file_name):
