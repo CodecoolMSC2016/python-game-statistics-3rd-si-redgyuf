ce5c17f5c624f1b1ee17e6c4fc445354df694945
Daniel Gal <faramirex070@gmail.com>
2016-11-26 13:31:52 +0100

    get_selling_avg() implemented - passed

--- part2/reports.py
+++ part2/reports.py
@@ -30,7 +30,17 @@ def sum_sold(file_name):
 
 
 def get_selling_avg(file_name):
-    pass
+    list_of_games = []
+    total_sold = 0
+
+    with open(file_name, mode="r") as my_file:
+        for lines in my_file:
+            list_of_games.append(lines.replace('\n', "").split(sep='\t'))
+
+    for i in range(0, len(list_of_games)):
+        total_sold += float(list_of_games[i][1])
+
+    return total_sold / len(list_of_games)
 
 
 def count_longest_title(file_name):
